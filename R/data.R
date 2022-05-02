78a15e074a6c9983b29ba483cc05882a11f21636
taylorgrant <taylorgrant38@gmail.com>
2022-05-02 10:53:09 -0700

    documenting data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' 770 customer reviews of a winter dog hoodie
+#'
+#' A dataset containing reviews - date, star rating, headline, and review text
+#' of the Jecikelon Winter Dog Hoody Sweater
+#'
+#' @format A data frame with 770 rows and 5 variables:
+#' \describe{
+#'   \item{date}{date of review}
+#'   \item{stars}{star rating of review, 1 to 5}
+#'   \item{headline}{review headline}
+#'   \item{text}{review text}
+#'   \item{link}{url to the page of the review}
+#'   ...
+#' }
+#' @source \url{http://www.amazon.com}
+"sample_reviews"
